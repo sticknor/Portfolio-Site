755668cd37715507a92f68d3b190e7a233f1f728
Sam Ticknor <sam@getoctane.io>
2023-10-17 10:23:51 -0400

    Updates

---
+++ precache-manifest.dc88308e0762d50754a0724e241787d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e7db3417e1dcb3c374d",
+    "url": "/static/css/main.320333a2.chunk.css"
+  },
+  {
+    "revision": "2e7db3417e1dcb3c374d",
+    "url": "/static/js/main.abf4683f.chunk.js"
+  },
+  {
+    "revision": "63328b9474d0c7aa895e",
+    "url": "/static/js/runtime~main.82adb90b.js"
+  },
+  {
+    "revision": "3a1323019a464e41c47b",
+    "url": "/static/js/2.f0181881.chunk.js"
+  },
+  {
+    "revision": "0b3c8090bf2ea0833981",
+    "url": "/static/js/3.2350105a.chunk.js"
+  },
+  {
+    "revision": "4cf6d121a084d0f7499593063ac43d58",
+    "url": "/static/media/splash.4cf6d121.jpg"
+  },
+  {
+    "revision": "0a84da04ac8796df87ba8c0587a1d7c8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40ead4e480899eb16f8e6902b0b1b684.js"
+  "/precache-manifest.dc88308e0762d50754a0724e241787d8.js"
 );
 
 workbox.clientsClaim();
